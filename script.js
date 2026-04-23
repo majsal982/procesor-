b9692d342c1d90f1ed6be73c6c60b22e9ec41d7a
SeishiUmaiIB <igor.brzezinski123@gmail.com>
2026-04-23 16:13:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// LOGIKA PROCESORA
 
+const processor = {
+  instruction: '',
+  argument: '',
+
+  set(instruction, argument) {
+    this.instruction = instruction;
+    this.argument = argument;
+    this.render();
+  },
+
+  clear() {
+    this.instruction = '';
+    this.argument = '';
+    this.render();
+  },
+
+  render() {
+    document.getElementById('instruction').value = this.instruction;
+    document.getElementById('argument').value = this.argument;
+  }
+}
+
+// KONIEC
